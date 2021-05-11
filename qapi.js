36ea7048d77151570ca094405817856231be7b7e
Goalof <68434460+Goalof@users.noreply.github.com>
2021-05-11 12:44:03 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"609a274c2061a7001e176c19",
+				"609a274c2061a7001e176c1b"
+			]
+		},
+		"609a274c2061a7001e176c19": {
+			"id": "609a274c2061a7001e176c19",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"609a274c2061a7001e176c1b": {
+			"id": "609a274c2061a7001e176c1b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as QuarklycommunityKitAnimation } from "./QuarklycommunityKitAnimation"
