85eb4463d94c56cccf29ebbc28d97f9a4b898e55
Goalof <68434460+Goalof@users.noreply.github.com>
2021-06-07 00:37:14 +0600

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Theme, Link, Text, Box, Section, Image, Input, Button, Icon } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { Override, Menu, Section } from "@quarkly/components";
+import { Override, Menu, StackItem, Stack, Formspree } from "@quarkly/components";
 import * as Components from "components";
+import { MdHelp } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -41,27 +42,232 @@ export default (() => {
 				</Menu>
 			</Box>
 		</Section>
-		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
-			<Components.QuarklycommunityKitAnimation animation="↑ Slide Out" test={false} duration="1.3s" timingFunction="linear">
-				<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Components.QuarklycommunityKitCollapse>
+				<Override slot="Button" width="100%" background="--color-secondary" color="--dark">
+					sadas
+				</Override>
+				<Text>
+					Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{"\n\n"}
+				</Text>
+			</Components.QuarklycommunityKitCollapse>
+			<Components.QuarklycommunityKitCollapse border-width="0px">
+				<Override
+					slot="Button"
+					width="100%"
+					background="--color-secondary"
+					color="--dark"
+					margin="20px 0px 50px 0px"
+				>
+					Первый контейнер
+				</Override>
+				<Text>
+					Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{"\n\n"}
+				</Text>
+			</Components.QuarklycommunityKitCollapse>
+			<Components.QuarklycommunityKitCollapse>
+				<Override slot="Button">
+					sadas
+				</Override>
+				<Text>
+					Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{"\n\n"}
+				</Text>
+			</Components.QuarklycommunityKitCollapse>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+			</Text>
+			<Components.QuarklycommunityKitAudio />
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503341960582-b45751874cf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://images.unsplash.com/photo-1503342394128-c104d54dba01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+			<Components.QuarklycommunityKitVimeo
+				video="https://vimeo.com/557592709"
+				autopause
+				autoplay
+				volume="0"
+				muted
+				playBackground={false}
+				controls={false}
+				sm-height="200px"
+			/>
+		</Section>
+		<Section background="--color-light" color="--dark" padding="64px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
 					<Text
-						as="h4"
 						font="--base"
 						color="--grey"
 						letter-spacing="1px"
 						text-transform="uppercase"
-						margin="6px 0"
+						margin="0"
 					>
-						About me
+						Contact
 					</Text>
-					<Text as="h2" font="--headline2" margin="0 0 12px 0">
-						Hey I’m David
+					<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+						Email us, we would love to hear form you
 					</Text>
-					<Text font="--base">
-						My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
-					</Text>
-				</Box>
-			</Components.QuarklycommunityKitAnimation>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Formspree>
+						<Stack gap="16px">
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Name
+								</Text>
+								<Input max-width="400px" width="100%" name="name" />
+							</StackItem>
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Email
+								</Text>
+								<Input max-width="400px" width="100%" type="email" name="email" />
+							</StackItem>
+							<StackItem width="100%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Message
+								</Text>
+								<Input as="textarea" rows="4" width="100%" name="message" />
+							</StackItem>
+							<StackItem width="100%">
+								<Button>
+									Send
+								</Button>
+							</StackItem>
+						</Stack>
+					</Formspree>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Components.QuarklycommunityKitVideo
+				playOnHover={false}
+				width="100%"
+				poster="https://images.unsplash.com/photo-1622915141329-882dc438577f?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000"
+				autoPlay={false}
+				muted={false}
+				loop
+				src="https://images.unsplash.com/photo-1622915141329-882dc438577f?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000"
+			/>
+			<Button font="--lead" margin="20px">
+				Button
+			</Button>
+		</Section>
+		<Section color="--dark">
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Q&A
+			</Text>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Is there life on Mars?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					How much salt is in the ocean?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box display="flex" justify-content="space-between">
+				<Components.QuarklycommunityKitCounter />
+				<Components.QuarklycommunityKitCounter direction="normal" startProp="0" />
+			</Box>
+			<Components.QuarklycommunityKitVkPage groupId="20003922" colorAccent="#3b5472" />
+			<Box
+				margin="36px 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Why is the sky blue?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+				<Components.QuarklycommunityKitPicture />
+				<Components.QuarklycommunityKitTable />
+				<Components.QuarklycommunityKitTable width="100%">
+					<Override slot="Text TBody Row-0 Col-5">
+						аыфвфывфыфывфыв
+					</Override>
+					<Override slot="TBody" width="100%" />
+					<Override slot="Cell" width="100%" />
+				</Components.QuarklycommunityKitTable>
+			</Box>
+			<Components.QuarklycommunityKitTimer
+				showDays={false}
+				showTextDone="complete"
+				showHours
+				showMinutes={false}
+				showSeconds
+			/>
 		</Section>
 		<Section background-color="--dark" text-align="center" padding="32px 0">
 			<Menu
@@ -77,6 +283,7 @@ export default (() => {
 				<Override slot="link-active" color="--primary" />
 				<Override slot="item" padding="6px 0px" />
 			</Menu>
+			<Components.QuarklycommunityKitDisqusComment />
 			<Link
 				href="mailto:hello@company.com"
 				text-decoration-line="none"
@@ -87,6 +294,10 @@ export default (() => {
 				hello@company.com
 			</Link>
 		</Section>
+		<Components.QuarklycommunityKitBackToTop />
+		<Components.QuarklycommunityKitScrollIndicator />
+		<Components.QuarklycommunityKitSoundCloud type="standart" url="https://soundcloud.com/octobersveryown/drake-wants-and-needs-feat-lil" />
+		<Components.QuarklycommunityKitSoundCloud type="visual" url="https://soundcloud.com/octobersveryown/drake-wants-and-needs-feat-lil" />
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
